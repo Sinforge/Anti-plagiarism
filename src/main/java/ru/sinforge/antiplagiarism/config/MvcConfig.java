8ff77fcb4589c67190c84b6aab5a68fb22ff5c61
Sinforge <vlad.vlasov77@mail.ru>
2022-05-12 14:57:47 +0300

    add resource handler

--- src/main/java/ru/sinforge/antiplagiarism/config/MvcConfig.java
+++ src/main/java/ru/sinforge/antiplagiarism/config/MvcConfig.java
@@ -1,11 +1,24 @@
 package ru.sinforge.antiplagiarism.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(
+                "classpath:/static/");
+    }
+
+
+
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/registration").setViewName("registration");
+        registry.addViewController("/auth").setViewName("auth");
     }
 }
